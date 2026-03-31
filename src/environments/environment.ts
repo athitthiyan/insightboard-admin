e93be82bd84691514b6874b617cff78736dcef02
athitthiyan <athitthiyananofficial@gmail.com>
2026-03-31 21:29:07 +0530

    update the url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000',
-  bookingAppUrl: 'http://localhost:4200',
-  paymentAppUrl: 'http://localhost:4201',
+  apiUrl: 'https://hotelapi-backend.onrender.com',
+  bookingAppUrl: 'https://stayease-booking-app.vercel.app/',
+  paymentAppUrl: 'https://payflow-payment-app.vercel.app/',
 };
