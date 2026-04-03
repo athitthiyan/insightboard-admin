1dbbd5e1c806ea887bf96943cb667261d7d188ae
athitthiyan <athitthiyananofficial@gmail.com>
2026-04-04 01:37:53 +0530

    updated environment

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://hotel-api-production-447d.up.railway.app/',
+  apiUrl: 'https://hotel-api-production-447d.up.railway.app',
   bookingAppUrl: 'https://stayease-booking-app.vercel.app',
   paymentAppUrl: 'https://payflow-gateway.vercel.app',
 };
